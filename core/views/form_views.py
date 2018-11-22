82d896ce31bb1fa6cb3cc1d062b0470f38123236
neveditsin <neveditsin@gmail.com>
2018-11-22 14:41:38 -0400

    redirect after submit

--- core/views/form_views.py
+++ core/views/form_views.py
@@ -1,47 +1,24 @@
-#from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 import csv
 import json
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import TemplateView
-from django.template.context_processors import request
 from core import models
-#from django.views.generic.edit import CreateView
 
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic.list import ListView
-from ..models import Criteria, Scale
-from ..forms import CriteriaForm, DynamicForm
-from django.urls import reverse_lazy
+
+from ..models import Criteria, Scale, JudgingRound
 from django.urls import reverse
-from django.http import Http404
+from django.http import HttpResponseRedirect
 
 
         
 class SumbitFormView(TemplateView):
     template_name = "core/form_create.html"
     
-    
     def post(self, request, *args, **kwargs):
         print(request.POST)
-#         c=Criteria(name=request.POST.get('name'))
-#         c.save()
-#         s=Criteria(scale=request.POST.get('scale'))
-#         s.save()
-        form = CriteriaForm(request.POST)
-        if form.is_valid():
-            form.create(name=form.cleaned_data["name"],scale=form.cleaned_data["scale"])
-#            form.save()
-#             name=form.cleaned_data['name']
-#             scale=form.cleaned_data['scale']
-#             p=Criteria(name=name, scale=scale)
-#             p.save()
 
-
-        #criteria_name = request.POST.get('name'+i)
-        return render(request, self.template_name, {'form': form})
+        return HttpResponseRedirect(self.get_success_url())
+        return reverse("core:hackathon_list")
     
     def load_names(self, file_path):
         reader = csv.DictReader(open(file_path))
@@ -67,12 +44,11 @@ class SumbitFormView(TemplateView):
         mimetype = 'application/json'
         return HttpResponse(data, mimetype)
     
-    def get_context_data(self, *args, **kwargs):
-        self.kwargs['jround_id']
+    def get_context_data(self, *args, **kwargs):        
         ret = super(SumbitFormView, self).get_context_data(*args, **kwargs)
         ret['scale'] = Scale.objects.all()
         return ret
     
-
-
-
+        
+    def get_success_url(self):
+        return reverse("core:judging_round_detail", kwargs={'pk':self.kwargs['jround_id']})
