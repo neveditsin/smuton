fbe24b42fa7fe37ef7fa9f8b3c0ee3cbd5f8aa72
neveditsin <neveditsin@gmail.com>
2018-11-29 15:33:30 -0400

    criteria views removed

--- core/urls/__init__.py
+++ core/urls/__init__.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^teams/', include('core.urls.team_urls')),
     url(r'^scales/', include('core.urls.scale_urls')),
     url(r'^scale_entries/', include('core.urls.scale_entry_urls')),
-    url(r'^criterias/', include('core.urls.criteria_urls')),
     url(r'^hackathons/', include('core.urls.hackathon_urls')),
     url(r'^judging_rounds/', include('core.urls.judging_round_urls')),
     url(r'^judge_responses/', include('core.urls.judge_response_urls')),
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -3,7 +3,6 @@ from .judge_views import *  # NOQA
 from .team_views import *  # NOQA
 from .scale_views import *  # NOQA
 from .scale_entry_views import *  # NOQA
-from .criteria_views import *  # NOQA
 from .hackathon_views import *  # NOQA
 from .judging_round_views import *  # NOQA
 from .judge_response_views import *  # NOQA
