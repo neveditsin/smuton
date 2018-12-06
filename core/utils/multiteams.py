dded7e6c54427e1a782e2c46d8b7129bf1a1249e
neveditsin <neveditsin@gmail.com>
2018-12-06 15:11:51 -0400

    draw table

--- core/utils/multiteams.py
+++ core/utils/multiteams.py
@@ -37,10 +37,48 @@ def mk_qrcode(boxsz, x,y):
     qr_code = qr.make_image(fill_color="black", back_color="white")
     img.paste(qr_code, (x,y))
     return(x,y,qr_code.size[0]+x,qr_code.size[1]+y)
-      
 
 
+def fit_count(header_height, t_sz, min_row_sz, row_count):
+    row_sz = (t_sz[1]-header_height)/row_count
+    if row_sz < min_row_sz:
+        row_sz = min_row_sz
+        #recalculate count
+        count_fit = math.floor((t_sz[1]-header_height)/min_row_sz)       
+        #recalculate row_size
+        row_sz = (t_sz[1]-header_height)/count_fit
+        #pages_count
+        pg_count = math.ceil(row_count/count_fit)
+        return(pg_count,count_fit)
+        
+ 
+def draw_table(x,y, t_sz, line_wdth, header_height, row_count, row_header_height, col_count):
+    draw.rectangle(((x, y), (x+t_sz[0], y+t_sz[1])), outline=0, width=line_wdth)
+    #horizontal
+    #header
+    header_height = header_height
+    draw.line(((x,y+header_height),(WIDTH-x,y+header_height)), fill=0, width=line_wdth)
+    
+    row_sz = (t_sz[1]-header_height)/row_count       
+    for i in range(1, row_count):
+        dy = math.floor(row_sz*i)
+        draw.line(((x,(y+header_height)+dy),(WIDTH-x,(y+header_height)+dy)), fill=0, width=line_wdth)
+     
+    #vertical
+    #row_lables
+    draw.line(((x+row_header_height,y),(x+row_header_height, y+t_sz[1])), fill=0, width=line_wdth)
+    
+    col_sz = (t_sz[0]-row_header_height)/col_count    
+    for i in range(1, col_count):
+        dx = math.floor(col_sz*i)
+        draw.line(((x+row_header_height+dx,y),(x+row_header_height+dx,y+t_sz[1])), fill=0, width=line_wdth)
+    
+    
 corners(CORNERS,100,10)
+
+draw_table(50,250,(WIDTH-50*2, HEIGHT-250-50),2,200,8, 300, 10)
+
+
 font = ImageFont.truetype(font="arial.ttf", size=50)
 #draw.text((200, 500), "hello", font=font, fill=0)
 #print(font.getsize("helloggggggggggggggggggggg"))
@@ -50,29 +88,10 @@ font = ImageFont.truetype(font="arial.ttf", size=50)
 #print(circle(30, 300, 600))
 print(mk_qrcode(8, CORNERS*2, CORNERS*2))
 
-x = 50
-y = 250
-t_sz = (WIDTH-x*2, HEIGHT-y-50) 
-draw.rectangle(((x, y), (x+t_sz[0], y+t_sz[1])), outline=0, width=2)
+      
+        
 
-#horizontal
-#header
-header_height = 150
-draw.line(((x,y+header_height),(WIDTH-x,y+header_height)), fill=0, width=2)
-#rows
-min_row_sz = 200
-row_count = 5
-row_sz = (t_sz[1]-header_height)/row_count
-if row_sz < min_row_sz:
-    print("size is too small. using minimal size")
-    row_sz = min_row_sz
-    #recalculate count
-    count_fit = math.floor((t_sz[1]-header_height)/min_row_sz)
-    print(count_fit)
-    count_fit = 10
-for i in range(1, row_count):
-    dy = math.floor(row_sz*i)
-    draw.line(((x,(y+header_height)+dy),(WIDTH-x,(y+header_height)+dy)), fill=0, width=2)
+
 
 
 img.save("C:\\temp\\image.png", "PNG")
