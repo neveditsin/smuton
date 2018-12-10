d35a373fd84f451a471d99699e9d7df8a9b77726
neveditsin <neveditsin@gmail.com>
2018-12-10 12:10:33 -0400

    making code object-oriented

--- core/utils/multiteams.py
+++ core/utils/multiteams.py
@@ -5,53 +5,23 @@ from PIL import ImageDraw
 import qrcode
 from fpdf import FPDF
 import math 
-from core.utils.template_creator import create_template
 
-
-WIDTH = 3300
-HEIGHT = 2550
-CORNERS = 25
-img = Image.new('1', (WIDTH, HEIGHT), 1)
-draw = ImageDraw.Draw(img)
-
-def corners(margin, size, width):
-    draw.rectangle([(margin, margin), (WIDTH-margin, HEIGHT-margin)], outline = 0, width=width)
-    draw.rectangle([(margin+size, 0), (WIDTH-margin-size, HEIGHT)], outline=1, fill=1)
-    draw.rectangle([(0, margin+size), (WIDTH, HEIGHT-margin-size)], outline=1, fill=1)
-    return ((margin, margin), (WIDTH-margin, margin), 
-            (margin, HEIGHT-margin), (WIDTH-margin, HEIGHT-margin),)
-    
-def square(sz, x, y):
-    wdth = 3
-    draw.rectangle(((x, y), (x+sz, y+sz)), outline=0, fill=1, width=wdth)
-    return(x+sz/2, y+sz/2)
-
-def circle(sz, x, y):
-    wdth = 3
-    draw.ellipse((x, y, x+sz, y+sz), fill=1, outline = 0, width=wdth)
-    return(x+(sz/2), y+(sz/2))
-
-def mk_qrcode(data,boxsz, x,y):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_M,
-        box_size=boxsz,
-        border=0)
-    qr.add_data(data)
-    qr.make(fit=True)  
-    qr_code = qr.make_image(fill_color="black", back_color="white")
-    img.paste(qr_code, (x,y))
-    return(x,y,qr_code.size[0]+x,qr_code.size[1]+y)
-
+##testing only
+import sys
+sys.path.append('..')
+##testing only END
+from core.utils import template_creator
 
 
 class Table:
     row_coords = []
     col_coords = []
+    draw = None
     
-    def __init__(self, x,y, t_sz, line_wdth, header_height, nrow, row_header_height, ncol):
+    def __init__(self, draw, x,y, t_sz, line_wdth, header_height, nrow, row_header_height, ncol):
+        self.draw = draw
         self.draw_table(x,y, t_sz, line_wdth, header_height, nrow, row_header_height, ncol)
-        
+
     
     def fit_count(self, header_height, t_sz, min_row_sz, nrow):
         row_sz = (t_sz[1]-header_height)/nrow
@@ -66,30 +36,30 @@ class Table:
             return(pg_count,count_fit)
         
     def draw_table(self, x,y, t_sz, line_wdth, header_height, nrow, row_header_height, ncol):
-        draw.rectangle(((x, y), (x+t_sz[0], y+t_sz[1])), outline=0, width=line_wdth)
+        self.draw.rectangle(((x, y), (x+t_sz[0], y+t_sz[1])), outline=0, width=line_wdth)
         #horizontal
         #header
         header_height = header_height
-        draw.line(((x,y+header_height),(WIDTH-x,y+header_height)), fill=0, width=line_wdth)
+        self.draw.line(((x,y+header_height),(x+t_sz[0],y+header_height)), fill=0, width=line_wdth)
         
         row_sz = math.floor((t_sz[1]-header_height)/nrow)
         self.row_coords.append((y+header_height,row_sz))
         for i in range(1, nrow):
             dy = math.floor(row_sz*i)
             _y = (y+header_height)+dy
-            draw.line(((x,_y),(WIDTH-x,_y)), fill=0, width=line_wdth)
+            self.draw.line(((x,_y),(x+t_sz[0],_y)), fill=0, width=line_wdth)
             self.row_coords.append((_y,row_sz))
          
         #vertical
         #row_lables
-        draw.line(((x+row_header_height,y),(x+row_header_height, y+t_sz[1])), fill=0, width=line_wdth)
+        self.draw.line(((x+row_header_height,y),(x+row_header_height, y+t_sz[1])), fill=0, width=line_wdth)
         
         col_sz = math.floor((t_sz[0]-row_header_height)/ncol)
         self.col_coords.append((x+row_header_height,col_sz))
         for i in range(1, ncol):
             dx = math.floor(col_sz*i)
             _x = x+row_header_height+dx
-            draw.line(((_x,y),(_x,y+t_sz[1])), fill=0, width=line_wdth)
+            self.draw.line(((_x,y),(_x,y+t_sz[1])), fill=0, width=line_wdth)
             self.col_coords.append((_x,col_sz))
 
 
@@ -101,7 +71,8 @@ class Field:
     field_size = 0
     min_bw = 5
     bw = 0
-    def __init__(self, font, field_size, label_pos, possible_values):
+    def __init__(self, font, draw, field_size, label_pos, possible_values):
+        self.draw = draw
         self.field_size = field_size
         self.font = font
         self.textbox_sz = (max([font.getsize(val)[0] for val in possible_values]),
@@ -109,103 +80,144 @@ class Field:
         self.label_pos = label_pos
         if (self.label_pos == 'L'):
             self.bw = math.ceil(self.textbox_sz[0]) + self.min_bw
+            
+    def square(self, sz, x, y):
+        wdth = 3
+        self.draw.rectangle(((x, y), (x+sz, y+sz)), outline=0, fill=1, width=wdth)
+        return(x+sz/2, y+sz/2)
+    
+    def circle(self, sz, x, y):
+        wdth = 3
+        self.draw.ellipse((x, y, x+sz, y+sz), fill=1, outline = 0, width=wdth)
+        return(x+(sz/2), y+(sz/2))
         
     def do_draw(self, label, x, y):
         if (self.label_pos == 'L'):
-            draw.text((x, y), label, font=self.font, fill=0)
-            coords = circle(self.field_size, math.ceil(x+self.bw), y)
+            self.draw.text((x, y), label, font=self.font, fill=0)
+            coords = self.circle(self.field_size, math.ceil(x+self.bw), y)
             return coords
     
     def get_size(self):
         if (self.label_pos == 'L'):
             return (math.ceil(self.textbox_sz[0])+self.bw+self.field_size, max(math.ceil(self.textbox_sz[1]),self.field_size))
+
+
+
+class MultientryPaperForm:
+    WIDTH = 3300 #hardcoded for now
+    HEIGHT = 2550 #hardcoded for now
+    CORNERS = 25 #hardcoded for now
+    img = Image.new('1', (WIDTH, HEIGHT), 1)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font="arial.ttf", size=30)
+    t = None
+    template = None
+   
+    
+    def __init__(self,qr_info,ncol,nrow):
+        groups = []
+        
+        cornrs = self.corners(self.CORNERS,100,10)
+        self.t = Table(self.draw, 50,250,(self.WIDTH-50*2, self.HEIGHT-250-50),2,200,nrow,300,ncol)
+        qr_coords = self.mk_qrcode(qr_info, 8, self.CORNERS*2, self.CORNERS*2) 
+        qr_group = template_creator.create_qr(qr_info, (qr_coords[0],qr_coords[1]),
+                                    (qr_coords[0]+qr_coords[2], qr_coords[1]),
+                                    (qr_coords[0], qr_coords[1]+qr_coords[3]),
+                                    (qr_coords[0]+qr_coords[2], qr_coords[1]+qr_coords[3]))
+        groups.append(qr_group)
+        
+        for rc in self.t.row_coords:    
+            for cc in self.t.col_coords[0:1]:
+                entries =  ("1","2","3","4")
+                coords = self.draw_entries(rc,cc,True, 15, 10, 30,entries)
+                resps = []        
+                for c in coords:
+                    resps.append(template_creator.create_resp(c[0], c[1][0], c[1][1]))
+                groups.append(template_creator.create_group("GRP"+str(rc), resps))
+
+        self.template = template_creator.create_template(cornrs[0], cornrs[1], cornrs[2], cornrs[3],groups,30)
+        
+              
+    def save_as_image(self,path): #"C:\\temp\\image.png"
+        self.img.save(path, "PNG")
+    
+    def corners(self, margin, size, width):
+        self.draw.rectangle([(margin, margin), (self.WIDTH-margin, self.HEIGHT-margin)], outline = 0, width=width)
+        self.draw.rectangle([(margin+size, 0), (self.WIDTH-margin-size, self.HEIGHT)], outline=1, fill=1)
+        self.draw.rectangle([(0, margin+size), (self.WIDTH, self.HEIGHT-margin-size)], outline=1, fill=1)
+        return ((margin, margin), (self.WIDTH-margin, margin), 
+                (margin, self.HEIGHT-margin), (self.WIDTH-margin, self.HEIGHT-margin),)
         
     
-
-def max_entries(avail, obj_sz, min_dist):
-    return math.floor(avail/(obj_sz+min_dist))
-
-def break_entries(n_entries, max_entries):
-    if(n_entries > max_entries*2):
-        raise ValueError('NCE001: Connot fit entries into cell with current configuration')
-    d1 = math.floor(n_entries/2)
-    d2 = n_entries - d1
-    return(d2,d1)
-
-
-
-def draw_entries(rc, cc, vertical, min_dist, margin, fld_sz, entries_list):
-    sz = (rc[1] if vertical else cc[1])-margin*2
-    if(vertical):
-        f = Field(font, fld_sz, 'L', entries_list)
-        obj_sz=f.get_size()[1]
-    else:
-        f = Field(font, fld_sz, 'L', entries_list)
-        obj_sz=f.get_size()[0]  
+    def mk_qrcode(self,data,boxsz, x,y):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_M,
+            box_size=boxsz,
+            border=0)
+        qr.add_data(data)
+        qr.make(fit=True)  
+        qr_code = qr.make_image(fill_color="black", back_color="white")
+        self.img.paste(qr_code, (x,y))        
+        return (x,y,qr_code.size[0]+x,qr_code.size[1]+y)
+      
+    
+    def max_entries(self, avail, obj_sz, min_dist):
+        return math.floor(avail/(obj_sz+min_dist))
+    
+    def __break_entries(self, n_entries, max_entries):
+        if(n_entries > max_entries*2):
+            raise ValueError('NCE001: Connot fit entries into cell with current configuration')
+        d1 = math.floor(n_entries/2)
+        d2 = n_entries - d1
+        return(d2,d1)
+         
+  
+    def draw_entries(self, rc, cc, vertical, min_dist, margin, fld_sz, entries_list):
+        sz = (rc[1] if vertical else cc[1])-margin*2
+        if(vertical):
+            f = Field(self.font, self.draw, fld_sz, 'L', entries_list)
+            obj_sz=f.get_size()[1]
+        else:
+            f = Field(self.font, fld_sz, 'L', entries_list)
+            obj_sz=f.get_size()[0]  
+            
+        max_ent = self.max_entries(sz,obj_sz,min_dist)
+        n_entries = len(entries_list)
+        if(n_entries > max_ent):
+            ent = self.__break_entries(n_entries, max_ent)              
+        else:
+            ent = [n_entries]
+                    
+        block = 0
+        entry_idx = 0
+        coords = []
+        for nent in ent:    
+            dist = math.floor(sz/nent - obj_sz)
+            offset = ((rc[1] if vertical else cc[1])-((dist+obj_sz)*nent - dist))/2
+            for i in range(0, nent):
+                if (vertical):
+                    coords.append((entries_list[entry_idx], f.do_draw(entries_list[entry_idx], cc[0]+margin+block, rc[0]+offset+(dist+obj_sz)*i)))
+                else:
+                    coords.append((entries_list[entry_idx], f.do_draw(entries_list[entry_idx], cc[0]+offset+(dist+obj_sz)*i, rc[0]+margin+block)))
+                entry_idx+=1
         
-    max_ent = max_entries(sz,obj_sz,min_dist)
-    n_entries = len(entries_list)
-    if(n_entries > max_ent):
-        ent = break_entries(n_entries, max_ent)              
-    else:
-        ent = [n_entries]
-                
-    block = 0
-    entry_idx = 0
-    coords = []
-    for nent in ent:    
-        dist = math.floor(sz/nent - obj_sz)
-        offset = ((rc[1] if vertical else cc[1])-((dist+obj_sz)*nent - dist))/2
-        for i in range(0, nent):
-            if (vertical):
-                coords.append((entries_list[entry_idx], f.do_draw(entries_list[entry_idx], cc[0]+margin+block, rc[0]+offset+(dist+obj_sz)*i)))
-            else:
-                coords.append((entries_list[entry_idx], f.do_draw(entries_list[entry_idx], cc[0]+offset+(dist+obj_sz)*i, rc[0]+margin+block)))
-            entry_idx+=1
-    
-        block+= math.floor((cc[1] if vertical else rc[1])/2)
-    return coords
-    
+            block+= math.floor((cc[1] if vertical else rc[1])/2)
+        return coords
 
-
-
-cornrs = corners(CORNERS,100,10)
-t = Table(50,250,(WIDTH-50*2, HEIGHT-250-50),2,200,4, 300, 4)
-font = ImageFont.truetype(font="arial.ttf", size=30)
-
-import sys
-sys.path.append('..')
-from core.utils import template_creator
-groups = []
-for rc in t.row_coords:    
-    for cc in t.col_coords[0:1]:
-        entries =  ("1","2","3","4")
-        coords = draw_entries(rc,cc,True, 15, 10, 30,entries)
-        resps = []        
-        for c in coords:
-            resps.append(template_creator.create_resp(c[0], c[1][0], c[1][1]))
-        groups.append(template_creator.create_group("GRP"+str(rc), resps))
 
 #data '{0:010d}'.format(1)
-qr_coords = mk_qrcode('{0:010d}'.format(1),8, CORNERS*2, CORNERS*2)
-qr_template = template_creator.create_qr("hack_id", (qr_coords[0],qr_coords[1]),
-                                            (qr_coords[0]+qr_coords[2], qr_coords[1]),
-                                            (qr_coords[0], qr_coords[1]+qr_coords[3]),
-                                            (qr_coords[0]+qr_coords[2], qr_coords[1]+qr_coords[3]))
 
-groups.append(qr_template)      
-print(create_template(cornrs[0], cornrs[1], cornrs[2], cornrs[3],groups,30))
+
 
 
 #draw.text((200, 500), "hello", font=font, fill=0)
-#print(font.getsize("helloggggggggggggggggggggg"))
-
-#print(circle(30, 300, 600))
-
-img.save("C:\\temp\\image.png", "PNG")
+#print(font.getsize("hello"))
+form = MultientryPaperForm("hello", 4,8)
+form.save_as_image("C:\\temp\\image.png")
 
 
-pdf = FPDF(unit = "pt", format = [WIDTH, HEIGHT])
+pdf = FPDF(unit = "pt", format = [form.WIDTH, form.HEIGHT])
 pdf.add_page()
 pdf.image("C:\\temp\\image.png", 0, 0)
 pdf.output("C:\\temp\\form.pdf", "F")
